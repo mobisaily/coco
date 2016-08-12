f62ad1d2619dc729d19a02b1a2bc567d7bd949c9
sayali <sayali@macMachine.local>
2016-08-12 19:16:10 +0530

    resent proj

---
+++ Example/Pods/Target Support Files/Pods-testPod_Tests/Pods-testPod_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_testPod_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_testPod_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/testPod/testPod-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double testPodVersionNumber;
+FOUNDATION_EXPORT const unsigned char testPodVersionString[];
+
